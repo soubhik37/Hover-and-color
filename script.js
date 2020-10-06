b0db7758de0dd2d0a1d1c10c8437e18cba24ba5f
Soubhik Mondal <46368314+soubhik37@users.noreply.github.com>
2020-10-07 01:18:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function color1(){
+     document.getElementById('circle').style.backgroundColor="#05DDF0";
+}
+function color2(){
+     document.getElementById('circle').style.backgroundColor="#F16B04";
+}
+function color3(){
+     document.getElementById('circle').style.backgroundColor="#04F143";
+}
+function color4(){
+     document.getElementById('circle').style.backgroundColor="#3F3F3F";
+}
+function color5(){
+     document.getElementById('circle').style.backgroundColor="#9DFFF9";
+}
+function color6(){
+     document.getElementById('circle').style.backgroundColor="#ADD9F4";
+}
+function color7(){
+     document.getElementById('circle').style.backgroundColor="#468C98";
+}
+
